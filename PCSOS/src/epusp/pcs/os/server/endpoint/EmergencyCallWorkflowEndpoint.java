d77052a94ccdc79854e071dc147dfa1021bd9630
mhirami <stmidori@gmail.com>
2014-10-19 17:10:03 -0200

    Services updated to use encoded key

--- PCSOS/src/epusp/pcs/os/server/endpoint/EmergencyCallWorkflowEndpoint.java
+++ PCSOS/src/epusp/pcs/os/server/endpoint/EmergencyCallWorkflowEndpoint.java
@@ -5,12 +5,15 @@ import javax.inject.Named;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 
 import epusp.pcs.os.server.workflow.EmergencyCallWorkflow;
 import epusp.pcs.os.shared.model.AgentCollection;
 import epusp.pcs.os.shared.model.oncall.EmergencyCall;
 import epusp.pcs.os.shared.model.oncall.Position;
 import epusp.pcs.os.shared.model.person.user.Agent;
+import epusp.pcs.os.shared.model.vehicle.Vehicle;
 
 @Api(name = "emcallworkflowendpoint", namespace = @ApiNamespace(ownerDomain = "pcs.epusp", ownerName = "pcs.epusp", packagePath = "os.workflow"))
 public class EmergencyCallWorkflowEndpoint {
@@ -30,26 +33,34 @@ public class EmergencyCallWorkflowEndpoint {
 
 	@ApiMethod(name="addFreeVehicle")
 	public void addFreeVehicle(@Named("vehicleId") String vehicleId, AgentCollection agents) throws Exception {
-		instance.addFreeVehicle(vehicleId, agents.getAgentCollection());
+		Key k = KeyFactory.createKey(Vehicle.class.getSimpleName(), vehicleId);
+		String strK = KeyFactory.keyToString(k);
+		instance.addFreeVehicle(strK, agents.getAgentCollection());
 	}
 
 	@ApiMethod(name="updatePositionAndVerifyStatus")
 	public EmergencyCall updatePositionAndVerifyStatus(@Named("vehicleId") String vehicleId, Position position) {
-		instance.addVehiclePosition(vehicleId, position);
-		if(instance.isVehicleOnCall(vehicleId))
-			return instance.getVehicleEmergencyCall(vehicleId);
+		Key k = KeyFactory.createKey(Vehicle.class.getSimpleName(), vehicleId);
+		String strK = KeyFactory.keyToString(k);
+		instance.addVehiclePosition(strK, position);
+		if(instance.isVehicleOnCall(strK))
+			return instance.getVehicleEmergencyCall(strK);
 		else
 			return null;
 	}
 
 	@ApiMethod(name="ackVehicleOnCall")
 	public void ackVehicleOnCall(@Named("vehicleId") String vehicleId) {
-		instance.vehicleOnCallAcknowledgment(vehicleId);
+		Key k = KeyFactory.createKey(Vehicle.class.getSimpleName(), vehicleId);
+		String strK = KeyFactory.keyToString(k);
+		instance.vehicleOnCallAcknowledgment(strK);
 	}
 	
 	@ApiMethod(name="ackVehicleFinishedCall")
 	public void ackVehicleFinishedCall(@Named("vehicleId") String vehicleId) {
-		instance.vehicleFinishedCallAcknowledgment(vehicleId);
+		Key k = KeyFactory.createKey(Vehicle.class.getSimpleName(), vehicleId);
+		String strK = KeyFactory.keyToString(k);
+		instance.vehicleFinishedCallAcknowledgment(strK);
 	}
 	
 	@ApiMethod(name="addAgentsToVehicle")
@@ -59,16 +70,22 @@ public class EmergencyCallWorkflowEndpoint {
 
 	@ApiMethod(name="addAgentToVehicle")
 	public void addAgentToVehicle(@Named("vehicleId") String vehicleId, Agent agent) {
-		instance.addAgentToVehicle(vehicleId, agent);
+		Key k = KeyFactory.createKey(Vehicle.class.getSimpleName(), vehicleId);
+		String strK = KeyFactory.keyToString(k);
+		instance.addAgentToVehicle(strK, agent);
 	}
 	
 	@ApiMethod(name="removeAllAgentsFromVehicle")
 	public void removeAllAgentsFromVehicle(@Named("vehicleId") String vehicleId) {
-		instance.removeAllAgentsFromVehicle(vehicleId);
+		Key k = KeyFactory.createKey(Vehicle.class.getSimpleName(), vehicleId);
+		String strK = KeyFactory.keyToString(k);
+		instance.removeAllAgentsFromVehicle(strK);
 	}
 
 	@ApiMethod(name="removeAgentFromVehicle")
 	public void removeAgentFromVehicle(@Named("vehicleId") String vehicleId, Agent agent) {
-		instance.removeAgentFromVehicle(vehicleId, agent);
+		Key k = KeyFactory.createKey(Vehicle.class.getSimpleName(), vehicleId);
+		String strK = KeyFactory.keyToString(k);
+		instance.removeAgentFromVehicle(strK, agent);
 	}
 }
