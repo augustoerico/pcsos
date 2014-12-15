c308e5db398c178ec354060da1f325c396853dbf
GiovanniGatti <giovanni.gatti.pinheiro@gmail.com>
2014-12-15 18:12:09 -0200

    Atualizando propriedades da Vítima

--- PCSOS/src/epusp/pcs/os/shared/model/person/victim/VictimCustomAttributes.java
+++ PCSOS/src/epusp/pcs/os/shared/model/person/victim/VictimCustomAttributes.java
@@ -4,7 +4,11 @@ import epusp.pcs.os.shared.model.ICustomAttributes;
 import epusp.pcs.os.shared.model.attribute.DataType;
 
 public enum VictimCustomAttributes implements ICustomAttributes{
-	IDADE("idade", DataType.INTEGER), COR_DOS_OLHOS("cor_dos_olhos", DataType.STRING);
+	EMERGENCY_CONTACT("emergency_contact", DataType.STRING_ARRAY), BIRTHDAY("birthday", DataType.DATE),
+	HEIGHT("height", DataType.FLOAT), WEIGHT("weight", DataType.FLOAT), MARITAL_STATUS("victim_marital_status", DataType.STRING),
+	EYE_COLOR("eye_color", DataType.STRING), ADDRESS("victim_address", DataType.STRING), ZIP_CODE("victim_zip_code", DataType.STRING),
+	DIABETIC("diabetic", DataType.BOOLEAN), CARDIO("cardio", DataType.BOOLEAN), BLOOD_TYPE("blood_type", DataType.STRING), CARMAKER("carmaker", DataType.STRING),
+	MODEL("model", DataType.STRING), PLATE("license_plate", DataType.STRING), CAR_COLOR("car_color", DataType.STRING), CONTACT("contact", DataType.STRING);
 	
 	String attributeName;
 	DataType dataType;
